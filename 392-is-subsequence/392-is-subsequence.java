c32e85dd2ae196f4c244c5c252c271ede1357d8e
Kishore Kumar Gupta <lucario170@gmail.com>
2022-09-01 09:38:41 +0530

    Time: 1 ms (92.38%), Space: 42.4 MB (23.97%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -7,7 +7,7 @@ class Solution {
                 if(s.charAt(i)==t.charAt(j)){
                     matchesFound++;
                     j++;
-                    break;
+                   break;
                 }
             }
         }
