67cb51b91ee78ca60dfcfa231a24ebd5fea76c41
Kishore Kumar Gupta <lucario170@gmail.com>
2022-09-01 13:20:10 +0530

    Time: 32 ms (8.63%), Space: 58.6 MB (5.07%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.java
@@ -0,0 +1,32 @@
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+         int n = mat.length;
+        int m = mat[0].length;
+        int[] output = new int[n*m];
+        Hashtable<Integer, ArrayList<Integer>> hashtable = new Hashtable<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int key = i+j;
+                if (hashtable.get(key) ==null) {
+                    hashtable.put(i + j, new ArrayList<>());
+                }
+                hashtable.get(key).add(mat[i][j]);
+            }
+        }
+        int index=0;
+        int keyIndex=0;
+        while(index!=m*n){
+            ArrayList<Integer> numberList = hashtable.get(keyIndex);
+            if(keyIndex++%2==0)
+                Collections.reverse(numberList);
+            for (Integer number : numberList)
+                output[index++] = number;
+        }
+
+    return output;
+    }
+}
